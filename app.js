7b64515ab4331b8ff24a16c0af0f986b5819fc70
tran200376589 <200376589@student.georgianc.on.ca>
2019-05-30 17:17:15 -0400

    Our initial single application

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send(`Hey 'dere world!`);
+}); 
+
+app.get('/about', (req, res) => {
+	res.send(`I like to walks on the beach.`)
+});
+
+app.listen(4000, () => console.log('Listening on 4000'));
